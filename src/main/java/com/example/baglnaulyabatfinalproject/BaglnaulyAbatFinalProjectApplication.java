75177d791a19e68b7710a12e051bc67747b16eed
Abat <abat.nagibat0r@gmail.com>
2026-05-14 13:41:41 +0500

    version 0.0.0. Создание проекта. Начало

---
+++ src/main/java/com/example/baglnaulyabatfinalproject/BaglnaulyAbatFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.baglnaulyabatfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaglnaulyAbatFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaglnaulyAbatFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baglnaulyabatfinalproject/BaglnaulyAbatFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baglnaulyabatfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaglnaulyAbatFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
